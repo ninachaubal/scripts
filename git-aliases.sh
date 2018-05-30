9999560d0f6e7cf19bacbcb05f65815b595e44d7
Nina Chaubal <826592+ninachaubal@users.noreply.github.com>
2018-05-30 10:54:38 -0700

    Add git like

--- git-aliases.sh
+++ git-aliases.sh
@@ -9,3 +9,5 @@ git config --global alias.feature '!f() { git checkout master; git branch $1; gi
 git config --global alias.sync '!git fetch upstream; git checkout master; git merge upstream/master'
 
 git config --global alias.rebase '!f() { git checkout $1; git rebase master; }; f'
+
+git config --global alias.like '!f() { git reset --hard $1; }; f'

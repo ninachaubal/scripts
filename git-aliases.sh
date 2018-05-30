532f61423bdfc1e6b6483f206b87b84e5d68a94a
Nina Chaubal <826592+ninachaubal@users.noreply.github.com>
2018-05-30 14:42:18 -0700

    Add undo

--- git-aliases.sh
+++ git-aliases.sh
@@ -11,3 +11,5 @@ git config --global alias.sync '!git fetch upstream; git checkout master; git me
 git config --global alias.rebase '!f() { git checkout $1; git rebase master; }; f'
 
 git config --global alias.like '!f() { git reset --hard $1; }; f'
+
+git config --global alias.undo 'git reset HEAD~1'

63bce5018479efec192cbe522c324c1a054cf1c3
Nina Chaubal <826592+ninachaubal@users.noreply.github.com>
2018-12-06 19:37:05 -0800

    Update git-aliases.sh

--- git-aliases.sh
+++ git-aliases.sh
@@ -4,10 +4,6 @@ git config --global alias.unstage 'reset HEAD --'
 
 git config --global alias.last 'log -1 HEAD'
 
-git config --global alias.feature '!f() { git branch $1; git checkout $1; }; f'
-
-git config --global alias.sync-master '!git fetch upstream; git checkout master; git merge upstream/master'
-
 git config --global alias.like '!f() { git reset --hard $1; }; f'
 
 git config --global alias.undo 'reset HEAD~1'
